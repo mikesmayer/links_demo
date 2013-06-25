ae9afafd228d51f839e4e20a47e1c1f68daf098e
Mike Mayer <mayerms@gmail.com>
2013-06-25 16:06:53 -0400

    made some changes

--- main.rb
+++ main.rb
@@ -5,3 +5,22 @@ require 'sinatra/reloader' if development?
 get '/' do
   erb :home
 end
+
+get '/manual_links' do
+  @links = ['http://redit.com',
+            'http://news.ycombinator.com',
+            'http://techcrunch.com',
+            'http://macrumors.com',
+            'http://gigaom.com']
+  erb :links
+end
+
+get '/inks' do
+  link_file = File.new('links.csv', 'r')
+  @links = []
+  link_file.each do |line|
+    @links << line.split(',')
+  end
+  link_file.close
+  erb :links
+end
