780989c02a6666ac11d61f1eb4ccffeb452e15d9
Mike Mayer <mayerms@gmail.com>
2013-06-25 16:39:35 -0400

    revised some files

--- main.rb
+++ main.rb
@@ -6,16 +6,10 @@ get '/' do
   erb :home
 end
 
-get '/manual_links' do
-  @links = ['http://redit.com',
-            'http://news.ycombinator.com',
-            'http://techcrunch.com',
-            'http://macrumors.com',
-            'http://gigaom.com']
-  erb :links
+get '/about' do
 end
 
-get '/inks' do
+get '/links' do
   link_file = File.new('links.csv', 'r')
   @links = []
   link_file.each do |line|
@@ -24,3 +18,20 @@ get '/inks' do
   link_file.close
   erb :links
 end
+
+post '/navigate' do
+  case params[:destination].downcase
+    when "links" then redirect to('/links')
+    when "about" then redirect to('/about')
+  end
+  redirect to("/")
+end
+
+# get '/manual_links' do
+#   @links = ['http://redit.com',
+#             'http://news.ycombinator.com',
+#             'http://techcrunch.com',
+#             'http://macrumors.com',
+#             'http://gigaom.com']
+#   erb :links
+# end
